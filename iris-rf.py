2797069737780ef5b3a700702e235e3e1f481943
Sourav <souravraj664@gmail.com>
2025-01-02 20:49:30 +0530

    add random forest exp

---
+++ iris-rf.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import  matplotlib.pyplot as plt
+import seaborn as sns
+
+
+import dagshub
+dagshub.init(repo_owner='sourav664', repo_name='mlflow-dagshub-demo', mlflow=True)
+
+
+
+mlflow.set_tracking_uri("https://dagshub.com/sourav664/mlflow-dagshub-demo.mlflow")
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+max_depth = 10
+n_estimators = 10
+
+mlflow.set_experiment("iris-rf")
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth , n_estimators=n_estimators)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    confusion = confusion_matrix(y_test, y_pred)
+    mlflow.log_params({"max_depth": max_depth, "n_estimators": n_estimators})
+    mlflow.log_metric("accuracy", accuracy)
+    # mlflow.log_metric("confusion_matrix", confusion)
+    # mlflow.sklearn.log_model(rf, "model")
+    plt.figure(figsize=(6, 6))
+    sns.heatmap(confusion, annot=True, fmt="d", cmap="Blues", xticklabels=iris.target_names, yticklabels=iris.target_names)
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
+    plt.savefig("confusion_matrix.png")
+    
+    mlflow.log_artifact("confusion_matrix.png")
+    
+    mlflow.log_artifact(__file__)
+    
+    mlflow.sklearn.log_model(rf, "random_forest_model")
+    
+    mlflow.set_tag("author", "rahul")
+    mlflow.set_tag('model','random_forest')
